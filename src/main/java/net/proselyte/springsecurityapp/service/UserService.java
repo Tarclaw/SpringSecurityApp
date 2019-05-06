4264e70d2395863c6b69acbad9d4aacf4917e1b9
Andrew <a.b.paliychuk@gmail.com>
2019-05-06 18:11:28 +0300

    initial commit

---
+++ src/main/java/net/proselyte/springsecurityapp/service/UserService.java
@@ -0,0 +1,18 @@
+package net.proselyte.springsecurityapp.service;
+
+import net.proselyte.springsecurityapp.model.User;
+
+/**
+ * Service class for {@link net.proselyte.springsecurityapp.model.User}.
+ *
+ * @author andrew
+ *
+ * @version 1.0
+ */
+
+public interface UserService {
+
+    void save(User user);
+
+    User findByUsername(String username);
+}
